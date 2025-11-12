45e345bd088b9959696c6026d69d050f7909cc35
PORTO129-LAP\IT Dev <aditya.candra@porto.co.id>
2025-11-12 09:48:13 +0700

    add log timestamp

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os
 from model.measurement import measure_sandals
+from datetime import datetime
 
 if __name__ == "__main__":
     img_path = "QC-Detector\\input\\temp_assets\\WhatsApp Image 2025-11-11 at 14.54.35 (1).jpeg"
     mm_per_px = None 
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    output_filename = f"measured_{timestamp}.png"
+    output_path = os.path.join("QC-Detector", "output", "log_output", output_filename)
 
     print("Measuring:", img_path)
     res = measure_sandals(
         img_path, 
         mm_per_px=mm_per_px,
         draw_output=True,
-        save_out="QC-Detector\\output\\log_output\\measured.png"
+        save_out=output_path
     )
     print("Results:", res)
